4b17baf3054325f0d8e73d3fd2fbd3d42edea79c
☃ pitr <pitr.vern@gmail.com>
2012-06-22 10:32:41 -0400

    bump to 0.0.5

--- lib/guard/sidekiq/version.rb
+++ lib/guard/sidekiq/version.rb
@@ -2,7 +2,7 @@ module Guard
   module SidekiqVersion
     MAJOR = 0     # api
     MINOR = 0     # features
-    PATCH = 4     # bug fixes
+    PATCH = 5     # bug fixes
     BUILD = nil   # beta, rc1, etc
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
